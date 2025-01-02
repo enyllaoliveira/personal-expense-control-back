3e806d25c26ef502db7e9fa3ced27125882a90e2
enyllaoliveira <enyllaoliveira@gmail.com>
2025-01-02 09:35:01 -0300

    route fixed

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,11 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
+  console.log("Cookies recebidos:", req.cookies);
+
   const token = req.cookies.token;
-  if (!token) return res.status(403).json({ message: "Token não encontrado." });
+  if (!token) {
+    return res.status(403).json({ message: "Token não encontrado." });
+  }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: "Token inválido." });
+    if (err) {
+      return res.status(403).json({ message: "Token inválido." });
+    }
 
     req.userId = decoded.id;
     next();
